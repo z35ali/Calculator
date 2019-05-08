e2e2e9e0444caeca337e37450addbf255223063d
Zafar <zafar.a.ali2@gmail.com>
2019-05-08 15:18:08 -0400

    Add NEG button and functionality

--- app/src/main/java/com/ali/zafar/calculator/MainActivity.java
+++ app/src/main/java/com/ali/zafar/calculator/MainActivity.java
@@ -107,6 +107,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        Button buttonNeg = findViewById(R.id.buttonNeg);
+
+        buttonNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value = newNumber.getText().toString();
+                if(value.length() == 0) {
+                    newNumber.setText("-");
+                } else {
+                    try {
+                        Double doubleValue = Double.valueOf(value);
+                        doubleValue *= -1;
+                        newNumber.setText(doubleValue.toString());
+                    } catch(NumberFormatException e) {
+                        // newNumber was "-" or ".", so clear it
+                        newNumber.setText("");
+                    }
+                }
+
+            }
+        });
+
     }
 
     @Override
